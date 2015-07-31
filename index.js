db19bfaca083b3de15ff44f8b4421b86c4b4e933
Dominic Jodoin <cotsog@gmail.com>
2015-07-30 22:14:03 -0400

    Try printing something from the visited web page.

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ new Nightmare()
     .goto('http://yahoo.com')
     .type('input[title="Search"]', 'github nightmare')
     .click('.searchsubmit')
-    .run(function(err, nightmare) {
-        if (err) return console.log(err);
-        console.log('Done!');
-    });
+    .wait('.url.breadcrumb')
+    .evaluate(function() {
+        return document.querySelector('.url.breadcrumb').innerText;
+    }, function(breadcrumb) {
+        console.log(breadcrumb);
+    })
+    .run(done);
